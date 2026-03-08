1b3c5d00a78683edca6a0ee7ef5496f55c8908c3
DevarathN <devarathnediyirippil@gmail.com>
2026-03-08 14:08:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,12 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use(cors(origin:"https://pop-x-pi-six.vercel.app"));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
